b443859c9e4e14e09a969620e62bb656e9ad4b09
KarloFab <fabijanickarlo1@gmail.com>
2020-02-01 13:20:43 +0100

    Added notes repository

---
+++ src/main/kotlin/com/karlofab/notesapi/repository/NotesRepository.kt
@@ -0,0 +1,8 @@
+package com.karlofab.notesapi.repository
+
+import com.karlofab.notesapi.domain.Note
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.rest.core.annotation.RepositoryRestResource
+
+@RepositoryRestResource
+interface NotesRepository : JpaRepository<Note, Long>
